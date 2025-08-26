f6cd5a3b3f95cc8a976a294438cc993759414818
juuliaaj <juliajardim765@gmail.com>
2025-08-25 23:53:25 -0300

    :lipstick: Novo ícone de busca no mapa

--- frontend/src/pages/Mapeamento.jsx
+++ frontend/src/pages/Mapeamento.jsx
@@ -291,7 +291,7 @@ export default function Mapeamento() {
         <div className={styles.searchContainer}>
           <div className={styles.searchBox}>
             <span className={styles.searchIcon} aria-hidden>
-              🔎
+              <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-search"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" /><path d="M21 21l-6 -6" /></svg>
             </span>
             <input
               type="text"
